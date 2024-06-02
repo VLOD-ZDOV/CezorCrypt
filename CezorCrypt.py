95bd1606ac65dea8f4bd3158c4bb42d974a0bde5
Vlodimir <66350035+VLOD-ZDOV@users.noreply.github.com>
2024-06-02 13:24:56 +0700

    Add files via upload

--- CezorCrypt.py
+++ CezorCrypt.py
@@ -2,6 +2,7 @@
 #text - текст для расшифровки
 #shift - сдвиг 1-25 / 1-32
 #alphabet - алфавит en/ru
+
 def caesar_cipher(text, shift, alphabet):
     result = ''
     for char in text:
@@ -33,4 +34,5 @@ def main():
     alphabet = choose_alphabet()                      #Переменная алфавит равна функции выбора алфавита
     text = input("Введите текст для расшифровки: ")   
     all_caesar_shifts(text, alphabet)                 #Передает в функцию текст и алфавит
-main()
+while True:
+    main()
